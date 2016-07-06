ae09e9a1bac17993794ba554d382c209c7d74c09
Manson Li <manson.li3307@gmail.com>
2016-07-06 22:16:18 +0800

    personal info

--- app/src/main/java/com/littlesparkle/growler/raptor/driver/fragment/BaseFragment.java
+++ app/src/main/java/com/littlesparkle/growler/raptor/driver/fragment/BaseFragment.java
@@ -2,5 +2,26 @@ package com.littlesparkle.growler.raptor.driver.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.littlesparkle.growler.raptor.driver.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class BaseFragment extends Fragment {
+    protected void showDialogWithMessage(
+            SweetAlertDialog.OnSweetClickListener listener,
+            final String message) {
+        if (listener == null) {
+            listener = new SweetAlertDialog.OnSweetClickListener() {
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                    sweetAlertDialog.dismissWithAnimation();
+                }
+            };
+        }
+        new SweetAlertDialog(getContext(), SweetAlertDialog.WARNING_TYPE)
+                .setTitleText(message)
+                .setConfirmText(getString(R.string.ok))
+                .setConfirmClickListener(listener)
+                .show();
+    }
 }
