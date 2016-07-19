7ddd1b995863d76c8e9eac277b576a038b8665f2
Manson Li <manson.li3307@gmail.com>
2016-07-19 19:03:32 +0800

    do register activity

--- app/src/main/java/com/littlesparkle/growler/raptor/driver/activity/RegisterActivity.java
+++ app/src/main/java/com/littlesparkle/growler/raptor/driver/activity/RegisterActivity.java
@@ -1,13 +1,12 @@
 package com.littlesparkle.growler.raptor.driver.activity;
 
 import com.littlesparkle.growler.library.activity.BaseRegisterActivity;
-import com.littlesparkle.growler.library.dialog.DialogHelper;
 import com.littlesparkle.growler.library.http.BaseHttpSubscriber;
 import com.littlesparkle.growler.library.http.DefaultResponse;
-import com.littlesparkle.growler.library.http.api.ApiException;
 import com.littlesparkle.growler.library.log.Logger;
 import com.littlesparkle.growler.library.misc.MiscHelper;
 import com.littlesparkle.growler.library.user.UserRequest;
+import com.littlesparkle.growler.library.user.UserSignUpResponse;
 
 public class RegisterActivity extends BaseRegisterActivity {
     @Override
@@ -19,21 +18,51 @@ public class RegisterActivity extends BaseRegisterActivity {
         if (!MiscHelper.checkPhoneNumber(phoneNumber)) {
             return;
         }
-        new UserRequest().signupSendSms(new BaseHttpSubscriber<DefaultResponse>() {
+        new UserRequest().signupSendSms(new BaseHttpSubscriber<DefaultResponse>(this, this) {
             @Override
-            protected void onError(ApiException err) {
-                Logger.log("#####", err.toString());
+            protected void onError(String message) {
+                super.onError(message);
             }
 
             @Override
             public void onNext(DefaultResponse defaultResponse) {
-                Logger.log("#####", defaultResponse.toString());
             }
         }, phoneNumber);
     }
 
     @Override
     protected void onRegisterClick() {
-        DialogHelper.showDialogWithMessage(this, "register");
+        String phoneNumber = mMobileInput.getText().toString();
+        if ("".equals(phoneNumber)) {
+            return;
+        }
+        if (!MiscHelper.checkPhoneNumber(phoneNumber)) {
+            return;
+        }
+
+        String pwd = mPwdInput.getText().toString();
+        String pwdConfirm = mPwdConfInput.getText().toString();
+        if ("".equals(pwd) || "".equals(pwdConfirm)) {
+            return;
+        }
+        if (!pwd.equals(pwdConfirm)) {
+            return;
+        }
+        if (!MiscHelper.checkPassword(pwd)) {
+            return;
+        }
+
+        new UserRequest().signup(new BaseHttpSubscriber<UserSignUpResponse>(this, this) {
+            @Override
+            protected void onError(String message) {
+                super.onError(message);
+                Logger.log("signup error: " + message);
+            }
+
+            @Override
+            public void onNext(UserSignUpResponse userSignUpResponse) {
+                Logger.log("User: " + userSignUpResponse.toString());
+            }
+        }, phoneNumber, pwd, "1234");
     }
 }
