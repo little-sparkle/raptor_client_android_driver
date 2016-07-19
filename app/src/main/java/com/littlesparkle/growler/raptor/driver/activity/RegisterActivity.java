ec57a2dd0f65d05971c097b29e706e30bcfeef87
Manson Li <manson.li3307@gmail.com>
2016-07-19 19:09:12 +0800

    temp checkin

--- app/src/main/java/com/littlesparkle/growler/raptor/driver/activity/RegisterActivity.java
+++ app/src/main/java/com/littlesparkle/growler/raptor/driver/activity/RegisterActivity.java
@@ -3,10 +3,10 @@ package com.littlesparkle.growler.raptor.driver.activity;
 import com.littlesparkle.growler.library.activity.BaseRegisterActivity;
 import com.littlesparkle.growler.library.http.BaseHttpSubscriber;
 import com.littlesparkle.growler.library.http.DefaultResponse;
-import com.littlesparkle.growler.library.log.Logger;
 import com.littlesparkle.growler.library.misc.MiscHelper;
 import com.littlesparkle.growler.library.user.UserRequest;
-import com.littlesparkle.growler.library.user.UserSignUpResponse;
+import com.littlesparkle.growler.library.user.driver.DriverRequest;
+import com.littlesparkle.growler.library.user.driver.DriverSignUpResponse;
 
 public class RegisterActivity extends BaseRegisterActivity {
     @Override
@@ -52,16 +52,9 @@ public class RegisterActivity extends BaseRegisterActivity {
             return;
         }
 
-        new UserRequest().signup(new BaseHttpSubscriber<UserSignUpResponse>(this, this) {
+        new DriverRequest().signup(new BaseHttpSubscriber<DriverSignUpResponse>(this, this) {
             @Override
-            protected void onError(String message) {
-                super.onError(message);
-                Logger.log("signup error: " + message);
-            }
-
-            @Override
-            public void onNext(UserSignUpResponse userSignUpResponse) {
-                Logger.log("User: " + userSignUpResponse.toString());
+            public void onNext(DriverSignUpResponse driverSignUpResponse) {
             }
         }, phoneNumber, pwd, "1234");
     }
