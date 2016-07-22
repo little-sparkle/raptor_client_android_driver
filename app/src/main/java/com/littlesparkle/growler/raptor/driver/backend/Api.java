ce067751b99d81de6bf9e056181056c013aba690
Manson Li <manson.li3307@gmail.com>
2016-07-22 09:23:26 +0800

    add a default web page

--- app/src/main/java/com/littlesparkle/growler/raptor/driver/backend/Api.java
+++ app/src/main/java/com/littlesparkle/growler/raptor/driver/backend/Api.java
@@ -1,8 +1,9 @@
 package com.littlesparkle.growler.raptor.driver.backend;
 
 public class Api {
-    public static final String APPLY_FOR_DRIVER = "http://www.baidu.com";
-    public static final String WALLET = "http://www.baidu.com";
-    public static final String MESSAGE_CENTER = "http://www.baidu.com";
-    public static final String MY_CAR = "http://www.baidu.com";
+    public static final String UNDER_DEVELOPING = "file:///android_asset/under_developing.html";
+    public static final String APPLY_FOR_DRIVER = UNDER_DEVELOPING;
+    public static final String WALLET = UNDER_DEVELOPING;
+    public static final String MESSAGE_CENTER = UNDER_DEVELOPING;
+    public static final String MY_CAR = UNDER_DEVELOPING;
 }
